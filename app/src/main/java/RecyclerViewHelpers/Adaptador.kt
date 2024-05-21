aa72018ffaa67c253ef83d60429d1b9d4a546e16
- <->
2024-05-21 15:18:58 -0600

    Botones editar a medias y eliminar completo

--- app/src/main/java/RecyclerViewHelpers/Adaptador.kt
+++ app/src/main/java/RecyclerViewHelpers/Adaptador.kt
@@ -1,5 +1,6 @@
 package RecyclerViewHelper
 
+import android.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -8,14 +9,40 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import modelo.claseConexion
 import modelo.classMascotas
 
 
 class Adaptador(private var Datos: List<classMascotas>) : RecyclerView.Adapter<ViewHolder>() {
 
-    fun ActualizarLista(nuevaLista: List <classMascotas>){
+    fun ActualizarLista(nuevaLista: List <classMascotas> ){
         Datos = nuevaLista
         notifyDataSetChanged()//notifica al recycle que hay datos nuevos
+    }
+
+    //////// TODO: Eliminar datos
+    fun eliminarDatos(nombreMascota: String, posicion: Int){
+
+        //Actualizar lista de datos y notificar al adaptador
+        val listaDatos = Datos.toMutableList()
+        listaDatos.removeAt(posicion)
+
+        GlobalScope.launch(Dispatchers.IO) {
+            //crear objeto clase conexion
+            val objConexion = claseConexion().cadenaConexion()
+
+            //crear variable con prepare statement
+            val deleteMascota =objConexion?.prepareStatement("delete from tbMascotas where nombreMascota = ?")!!
+            deleteMascota.setString(1, nombreMascota)
+            deleteMascota.executeUpdate()
+
+            val commit = objConexion?.prepareStatement("commit")!!
+            commit.executeUpdate()
+        }
+
+        Datos = listaDatos.toList()
+        notifyItemRemoved(posicion)
+        notifyDataSetChanged()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -27,8 +54,54 @@ class Adaptador(private var Datos: List<classMascotas>) : RecyclerView.Adapter<V
     override fun getItemCount() = Datos.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val producto = Datos[position]
-        holder.textView.text = producto.nombreMascota
-    }
+        val mascota = Datos[position]
+        holder.textView.text = mascota.nombreMascota
 
+        //todo: click icono eliminar
+        holder.btnBorrar.setOnClickListener() {
+
+            val context = holder.itemView.context
+
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle("Eliminar")
+            builder.setMessage("¿Desea eliminar la mascota?")
+
+            //Botones
+
+            builder.setPositiveButton("si") { dialog, which ->
+                eliminarDatos(mascota.nombreMascota, position)
+            }
+
+            builder.setNegativeButton("No") { dialog, which ->
+                dialog.dismiss()
+            }
+
+            val dialog = builder.create()
+            dialog.show()
+        }
+        //todo: click icono editar
+        holder.btnEditar.setOnClickListener() {
+
+            val context = holder.itemView.context
+
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle("Editar")
+            builder.setMessage("¿Desea editar la mascota?")
+
+            //Botones
+
+            builder.setPositiveButton("si") { dialog, which ->
+
+            }
+
+            builder.setNegativeButton("No") { dialog, which ->
+                dialog.dismiss()
+            }
+
+            val dialog = builder.create()
+            dialog.show()
+        }
+
+    }
 }
+
--- app/src/main/java/RecyclerViewHelpers/ViewHolder.kt
+++ app/src/main/java/RecyclerViewHelpers/ViewHolder.kt
@@ -9,4 +9,6 @@ import jonathan.orellana.myapplication.R
 
 class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     val textView: TextView = view.findViewById(R.id.txtMascotaCard)
+    val btnEditar: ImageView = view.findViewById(R.id.imgEditar)
+    val btnBorrar: ImageView = view.findViewById(R.id.imgBorrar)
 }
